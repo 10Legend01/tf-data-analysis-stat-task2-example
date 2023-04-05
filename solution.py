9508866af3ab2dfee1daa7a7340ec010631a24de
10Legend01 <mihailnazarov01@mail.ru>
2023-04-06 00:13:31 +0300

    solution №3

--- solution.py
+++ solution.py
@@ -8,6 +8,13 @@ chat_id = 1379613676  # Ваш chat ID, не меняйте название п�
 
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = p
-    b_alpha = np.quantile(x, alpha)
-    return .059, b_alpha
+    alpha = 1 - p
+    const = 0.059
+    x -= const
+    return 2 * (x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))) + const, \
+           2 * (x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))) + const
+
+
+# x = np.array([0.3, 0.4, 0.45])
+
+# print(solution(0.95, x))

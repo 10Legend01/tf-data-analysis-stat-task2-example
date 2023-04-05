41d4c1ed1f7a87d21b13f59a10851ae11d126c2e
10Legend01 <mihailnazarov01@mail.ru>
2023-04-06 01:58:20 +0300

    solution №4.1

--- solution.py
+++ solution.py
@@ -11,8 +11,8 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     const = 0.059
     x -= const
-    return max(max(x), 2 * (x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)))) + const, \
-           max(max(x), 2 * (x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x)))) + const
+    return max(max(x), 2 * (x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))) + const), \
+           max(max(x), 2 * (x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))) + const)
 
 
 # x = np.array([0.1, 0.2, 0.3, 0.2, 0.15, 0.25])
